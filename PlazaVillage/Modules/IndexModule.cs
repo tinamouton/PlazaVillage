616d6cd534f74528b5b47300d97342e1d24d3404
tinamouton <tinamouton@gmail.com>
2013-06-30 22:17:52 -0500

    initial commit

---
+++ PlazaVillage/Modules/IndexModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace PlazaVillage.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+                {
+                    ViewBag.Title = "Plaza Village Condominiums";
+                    return View["index"];
+                };
+        }
+    }
+}
